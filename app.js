5e5fa84fb98e373805718add6633efa57256bc00
Ephraim Becker <emb@paay.co>
2022-11-17 14:22:00 -0500

    Fixd the bug.

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 /*
  * ensureAuthenticated Callback Function
 */
-const ensureAuthenticated = (req, res, next) => {
+function ensureAuthenticated(req, res, next) {
   if(req.isAuthenticated()) {
     return next();
   }
